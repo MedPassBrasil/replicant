411929c21d0bb8f8673fe777483e605def8835f7
hrozan <higorb.rozan@gmail.com>
2019-05-01 14:44:26 -0300

    change build dir name to "core"

--- src/components/atoms/List/index.js
+++ src/components/atoms/List/index.js
@@ -1,3 +1,3 @@
 export { default as ListItem } from "./ListItem"
-export { default as List } from "@material-ui/core/List"
-export { default as Divider } from "@material-ui/core/Divider"
+export List from "@material-ui/core/List"
+export Divider from "@material-ui/core/Divider"
