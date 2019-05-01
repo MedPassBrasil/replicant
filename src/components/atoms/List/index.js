2578bb484b2d885722147010931f1b686d91cfd6
hrozan <higorb.rozan@gmail.com>
2019-05-01 14:59:43 -0300

    fix List export

--- src/components/atoms/List/index.js
+++ src/components/atoms/List/index.js
@@ -1,5 +1,5 @@
 import List from "@material-ui/core/List"
 import Divider from "@material-ui/core/Divider"
 export { default as ListItem } from "./ListItem"
-export { List }
 export { Divider }
+export default List
