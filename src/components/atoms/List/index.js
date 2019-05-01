b8cf3a19c2f6ee8705fec8c20a6bf6beb58460cf
hrozan <higorb.rozan@gmail.com>
2019-05-01 14:48:01 -0300

    fix import syntax in List component

--- src/components/atoms/List/index.js
+++ src/components/atoms/List/index.js
@@ -1,3 +1,5 @@
+import List from "@material-ui/core/List"
+import Divider from "@material-ui/core/Divider"
 export { default as ListItem } from "./ListItem"
-export List from "@material-ui/core/List"
-export Divider from "@material-ui/core/Divider"
+export { List }
+export { Divider }
